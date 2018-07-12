748bbb2833dd4fcdfb7e616c848e1dfaa5a7cd2d
suzuki <suzuki@clive.matsuo.mech.keio.ac.jp>
2018-07-12 16:38:23 +0900

    [Add] toString

--- prc_shockHeight.f90
+++ prc_shockHeight.f90
@@ -4,9 +4,26 @@ module prc_shockHeight
 
   implicit none
 
+  real(4), allocatable :: wthh(:)
+
 contains
 
+subroutine setThreshold
+  integer :: i1
+  allocate(wthh(lscl))
+  write(lwrite,*) 'What is threshold of shock? Set each functions:'
+  do i1=1, lscl
+    write(lwrite,*) bscllst(i1)
+  enddo
+  read(lread, *) (wthh(i1), i1=1, lscl)
+  write(lwrite,*) 'Threshold is...'
+  do i1=1, lscl
+    write(lwrite,*) bscllst(i1)//' : '//toString(wthh(i1))
+  enddo
+endsubroutine
+
 subroutine shockHeight
+
 endsubroutine
 
 end module
